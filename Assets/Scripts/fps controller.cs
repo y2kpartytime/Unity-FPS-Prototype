4648b1e92b53c1f17da3f3158c85188a1fbacc2a
y2kpartytime <C24732831@myTUDublin.ie>
2024-12-10 10:52:05 +0000

    Update
    Added;
    -crouching
    -low poly guns
    -raycast to check above head while crouched
    -sprint

--- Assets/Scripts/fps controller.cs
+++ Assets/Scripts/fps controller.cs
@@ -8,14 +8,17 @@ public class fpscontroller : MonoBehaviour
     public bool CanMove { get; private set; } = true;
     private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
     private bool ShouldJump => Input.GetKey(jumpKey) && characterController.isGrounded;
+    private bool ShouldCrouch => Input.GetKey(crouchKey) && !duringCrouchAnimation;
 
     [Header("Functional Options")]
     [SerializeField] private bool canSprint = true;
     [SerializeField] private bool canJump = true;
+    [SerializeField] private bool canCrouch = true;
 
     [Header("Controls")]
     [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
     [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] private KeyCode crouchKey = KeyCode.LeftControl;
 
     [Header("Movement Parameters")]
     [SerializeField] private float walkSpeed = 3.0f;
@@ -30,7 +33,25 @@ public class fpscontroller : MonoBehaviour
     [Header("Jumping Parameters")]
     [SerializeField] private float jumpForce = 8.0f;
     [SerializeField] private float gravity = 30.0f;
+    
 
+    [Header("Crouching Parameters")]
+    [SerializeField] private float crouchHeight = 0.5f;
+    [SerializeField] private float standingHeight = 2f;
+    [SerializeField] private float timeToCrouch = 0.25f;
+    [SerializeField] private Vector3 crouchingCenter = new Vector3(0,0.5f,0);
+    [SerializeField] private Vector3 standingCenter = new Vector3(0,0,0);
+    private bool isCrouching;
+    private bool duringCrouchAnimation;
+
+
+    // Crouch height
+    // Stand height
+    // Is crouching
+    // Is is crouch animation
+    // Time to crouch/stand
+    // Standing center point
+    // Crouching center point
 
     private Camera playerCamera;
     private CharacterController characterController;
@@ -58,6 +79,9 @@ public class fpscontroller : MonoBehaviour
 
             if(canJump)
                 HandleJump();
+
+            if(canCrouch)
+                HandleCrouch();
             
             ApplyFinalMovements();
         }
@@ -93,5 +117,41 @@ public class fpscontroller : MonoBehaviour
     {
         if(ShouldJump)
             moveDirection.y = jumpForce;
+    }
+
+    private void HandleCrouch()
+    {
+        if (ShouldCrouch)
+            StartCoroutine(CrouchStand());
+    }
+
+    private IEnumerator CrouchStand()
+    {
+        if (isCrouching && Physics.Raycast(playerCamera.transform.position, Vector3.up, 1f))
+            yield break;
+
+        duringCrouchAnimation = true;
+
+        float timeElapsed = 0;
+        float targetHeight = isCrouching ? standingHeight : crouchHeight;
+        float currentHeight = characterController.height;
+        Vector3 targetCenter = isCrouching ? standingCenter : crouchingCenter;
+        Vector3 currentCenter = characterController.center;
+
+        while(timeElapsed < timeToCrouch)
+        {
+            characterController.height = Mathf.Lerp(currentHeight, targetHeight, timeElapsed / timeToCrouch);
+            characterController.center = Vector3.Lerp(currentCenter, targetCenter, timeElapsed / timeToCrouch);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        characterController.height = targetHeight;
+        characterController.center = targetCenter;
+
+        isCrouching = !isCrouching;
+
+        duringCrouchAnimation = false;
+
     }
 }
